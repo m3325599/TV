a14fcb8b3674e36c21214c814d76bd730e000392
m3325599 <m3325599@github.com>
2026-06-25 08:09:48 +0000

    Enhance video decoding: add media3-decoder-ffmpeg and prefer FFmpeg decoder

--- app/src/main/java/com/fongmi/android/tv/player/exo/ExoUtil.java
+++ app/src/main/java/com/fongmi/android/tv/player/exo/ExoUtil.java
@@ -91,7 +91,7 @@ public class ExoUtil {
     }
 
     private static int getRenderMode(int decode) {
-        return decode == PlayerEngine.HARD ? DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON : DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER;
+        return DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER;
     }
 
     private static CaptionStyleCompat getCaptionStyle() {
