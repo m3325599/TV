17ee006165bd3d6c131de8704d6a2cabd8d4a29e
m3325599 <m3325599@github.com>
2026-06-25 10:16:13 +0000

    Fix CustomPlayerView: use setSurfaceType instead of non-existent methods

--- app/src/main/java/com/fongmi/android/tv/ui/custom/CustomPlayerView.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/CustomPlayerView.java
@@ -2,12 +2,8 @@ package com.fongmi.android.tv.ui.custom;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.SurfaceView;
-import android.view.TextureView;
-import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.media3.common.Player;
 import androidx.media3.ui.PlayerView;
 import androidx.media3.ui.danmaku.DanmakuController;
 
@@ -34,6 +30,7 @@ public class CustomPlayerView extends PlayerView {
   private void init() {
     danmakuController = new DanmakuController(getContext());
     setUseController(false);
+    setSurfaceType(render == 1 ? SURFACE_TYPE_TEXTURE_VIEW : SURFACE_TYPE_SURFACE_VIEW);
   }
 
   public DanmakuController getDanmakuController() {
@@ -42,14 +39,7 @@ public class CustomPlayerView extends PlayerView {
 
   public void setRender(int render) {
     this.render = render;
-    Player player = getPlayer();
-    if (player == null) return;
-    setPlayer(null);
-    View surfaceView = getVideoSurfaceView();
-    View textureView = getVideoTextureView();
-    if (surfaceView != null) surfaceView.setVisibility(render == 0 ? VISIBLE : GONE);
-    if (textureView != null) textureView.setVisibility(render == 1 ? VISIBLE : GONE);
-    setPlayer(player);
+    setSurfaceType(render == 1 ? SURFACE_TYPE_TEXTURE_VIEW : SURFACE_TYPE_SURFACE_VIEW);
   }
 
   public int getRender() {
